544b092d03f51e7226e9fad011d3b582af9cb575
Kris-Amor <kamorstudy@gmail.com>
2025-04-15 23:01:47 +0800

    Set 4 done. Questions given. Adding more choices later

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -5,105 +5,105 @@ const setQ1 = [
     correct: "January 20, 2025"
 }, 
     {
-        question: "What does css stand for?",
+        question: "When did the Gaza Ceasefire take place?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What doese API stand for?",
+        question: "When did the South Korean President declare Martial Law?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
     
 }, 
     {
-        question: "what does ?",
+        question: "When did the South Korean President get impeached?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What?",
+        question: "When did Canadian Prime Minister Mark Carney want the snap election to take place? ",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }
 ]
 
 const set2 = [{
-    question: "What does HTML stand for?",
+    question: "What are the dominant major and minor parties in the midterm poll elections?",
     answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
     correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "According to COMELEC, how many Gen Z voters will vote in the 2025 midterm elections?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "When will the 2025 Philippine Senate election take place?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "What is the number of registered voters for 2025 according to COMELEC?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "Why do we use indelible ink, also called electoral ink, when voting?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }
 ]
 
 const set3 = [{
-    question: "What does HTML stand for?",
+    question: "When was former President Rodrigo Duterte arrested?",
     answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
     correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "When was the War on Drugs in the Philippines started?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "Who has Rodrigo Duterte in custody?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
+        question: "What was the major charge against Rodrigo Duterte?",
         answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
         correct: 0
 }, 
     {
-        question: "What does HTML stand for?",
-        answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-        correct: 0
+        question: "Where was Rodrigo Duterte taken?",
+        answers: ["The Hague in the Netherlands", "The windmill in the Netherlands", "The Hague in Russia", "The Hague with the Dutch"], 
+        correct: "The Hague in the Netherlands"
 }
 ]
 
 const set4 = [{
-    question: "What does HTML stand for?",
-    answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-    correct: 0
+    question: "How big is the budget for the 2025 fiscal year in the Philippines?",
+    answers: ["6.35 Trillion Pesos", "6.36 Trillion Pesos", "6.35 Billion Pesos", "7.35 Trillion Pesos"], 
+    correct: "6.35 Trillion Pesos"
 }, 
     {
-        question: "What does HTML stand for?",
-        answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-        correct: 0
+        question: "What is the national headline inflation rate for March 2025?",
+        answers: ["1.8 percent", "1.9 percent", "1.7 percent", "2.8 percent"], 
+        correct: "1.8 percent"
 }, 
     {
-        question: "What does HTML stand for?",
-        answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-        correct: 0
+        question: "According to PSA, what is the estimated unemployment rate of the Philippines in February 2025?",
+        answers: ["3.8 percent", "3.9 percent", "3.7 percent", "4.8 percent"], 
+        correct: "3.8 percent"
 }, 
     {
-        question: "What does HTML stand for?",
-        answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-        correct: 0
+        question: "According to PSA, what is the projected population of the Philippines on 2055?",
+        answers: ["138.67 Million", "138.69 Million", "139.67 Million", "138.67 Billion"], 
+        correct: "138.67 Million"
 }, 
     {
-        question: "What does HTML stand for?",
-        answers: ["Sample choice", "Sample choice", "Sample choice", "Sample choice"], 
-        correct: 0
+        question: "According to PSA, what is the full year GDP growth of the Philippines on 2055?",
+        answers: ["5.6 percent", "5.7 percent",	"5.5 percent", "6.6 percent"], 
+        correct: "5.6 percent"
 }
 ]
